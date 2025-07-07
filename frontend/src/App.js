3ba3a7895dd0b93d1f00b29f0549d9596063004d
Divyam <divyam.singh.2875@gmail.com>
2025-07-07 21:50:32 +0530

    new commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home'; // Import Home component
-import Login from './Login'; // Import Login component
+import Login from './components/Login'; // Import Login component
 import CarbonFootprint from './CarbonFootprint'; // Import CarbonFootprint component
 import './App.css'; // Import general styles
 
